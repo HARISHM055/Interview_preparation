6bde3115c86d4e9521b0c426bcdad6ef0b5771a1
Harish <harish635109@gmail.com>
2021-12-03 12:11:32 +0530

    two sum soln in java

---
+++ leetcode_probs/two_sum.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class two_sum
+{
+	public static void main(String[] args) {
+		HashMap<Integer,Integer> h = new HashMap<Integer,Integer>();
+		h.put(1,20);
+		h.put(2,21);
+		h.put(3,22);
+		h.put(4,23);
+		for(int i=1;i<=4;i++)
+		{
+		    System.out.println(h.get(i));
+		}
+	}
+}
